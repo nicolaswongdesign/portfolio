6cfc163ef484b33ca88018814303e061207c5943
nicolaswongdesign <121774374+nicolaswongdesign@users.noreply.github.com>
2024-02-11 19:13:31 -0600

    Add files via upload

--- hidden/valentine/script.js
+++ hidden/valentine/script.js
@@ -16,6 +16,17 @@ if (button != null) {
         button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
     };
 }
+
+if (button != null) {
+    button.addEventListener('click', myFunction);
+
+    function myFunction() {
+        button.style.position = 'absolute';
+        button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
+        button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
+    };
+}
+
 const buttonAgreed = document.getElementById('buttonTerms');
 const terms = document.getElementById('Terms');
 
